cfa04fcbfb501b322eabd21dbb408d804f2789c1
Michael Wenger <michaelwenger27@gmail.com>
2013-11-20 21:55:50 -0500

    Updated footer

--- include/bottom.php
+++ include/bottom.php
@@ -1,5 +1,5 @@
 </div>
-			<div id="footer" data-role="footer" style="background:#ECF6F8 url(''); height:4em;">
+			<div id="footer" data-role="footer" style="background:#ECF6F8 url(''); height: 100% !important;">
 				<div style="text-align:center";>
 					<img style="width:60%; display: block; margin: 0.5em auto;" src="images/general_motors_logo.png" />
 				</div>
