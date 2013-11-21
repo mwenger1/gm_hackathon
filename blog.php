083cc55e1faae0cfc99097437f7b3e60d4497f96
Michael Wenger <michaelwenger27@gmail.com>
2013-11-20 20:28:43 -0500

    Added map page with this year and all time options

--- blog.php
+++ blog.php
@@ -5,12 +5,36 @@
 			</div>
 
 			<div data-role="content">
-
-	<!-- 			<h2>States</h2> -->
+				<select id="mapTimeline">
+					<option value='year'>This Year</option>
+					<option value='alltime'>All Time</option>
+				</select>
 				<div style="text-align:center;">
-					<p class="mb2em" style="font-size:2em;">Your car has been to <br><span style="font-weight:bold; line-height:1.2em;">13 States!</span></p>
-					<img style="width:90%;" src="images/map_states.png" />
+					<p class="" style="font-size:2em; line-height:1.5em;">Your car has been to <br><span style="font-weight:bold; line-height:1.2em;" class="thisyear">13 States!</span>
+<span style="font-weight:bold; line-height:1.2em; display:none;" class="alltime">32 States!</span>
+					</p>
+
+					<img class="thisyear" style="width:90%;" src="images/map_states.png" />
+					<img class="alltime" style="width:90%; display:none;" src="images/map_states2.png" />
+
 				</div>
+
+<script>
+	$("#mapTimeline").change(function(){
+		var tmpValue = $(this).val();
+		if (tmpValue == "year"){
+			$(".alltime").hide();
+			$(".thisyear").show();
+
+		}
+		else if (tmpValue== "alltime"){
+			$(".alltime").show();
+			$(".thisyear").hide();
+
+		}
+	})
+</script>
+
 			
 			
 
