341677d67dc034bc0ece44621d08c1a0a6a56ac7
Nick Burwell <nburwell@gmail.com>
2013-11-20 22:51:25 -0500

    Added elevation to api

--- api.rb
+++ api.rb
@@ -13,5 +13,19 @@ vehicles = JSON.parse oauth_api['/account/vehicles?offset=0&size=3'].get(:Accept
 vehicles = vehicles["vehicles"]["vehicle"] or raise "No vehicles found"
 vehicle  = vehicles.find{ |v| v["model"] == "CTS Coupe" }
 
-puts telemetry = oauth_api["/account/vehicles/#{vehicle['vin']}/telemetry?begintimestamp=2012-10-01T09:45:00&endtimestamp=2013-11-01T09:45:00"].get(:Accept => "application/json", :Authorization => "#{token_type} #{access_token}")
+telemetry = JSON.parse oauth_api["/account/vehicles/#{vehicle['vin']}/telemetry?begintimestamp=2012-10-01T09:45:00&endtimestamp=2013-11-01T09:45:00"].get(:Accept => "application/json", :Authorization => "#{token_type} #{access_token}")
+
+data = telemetry["telemetries"]["vehicleData"]["telemetryData"]["telemetry"]
+lat_long = data.map do |d| 
+  a = d.split(" ");
+  el_api = JSON.parse(RestClient.get("http://maps.googleapis.com/maps/api/elevation/json?locations=#{a[1]},#{a[2]}&sensor=true"))
+  
+  { :timestamp => a[0],
+    :lat       => a[1],
+    :long      => a[2],
+    :elevation => el_api["results"].first["elevation"]
+  }
+end
+
+puts lat_long.to_yaml
 
